c40d72febf9da4d60ad022e96ddf1727f1f5152b
WeryZebra-Yue <ayushmaster100@gmail.com>
2022-10-04 19:10:07 +0530

    Added Javascript

---
+++ projects/Ecommerce Website/script.js
@@ -0,0 +1,9 @@
+const close = document.querySelector(".close");
+const open = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+close.addEventListener("click", () => {
+  menu.style.visibility = "hidden";
+});
+open.addEventListener("click", () => {
+  menu.style.visibility = "visible";
+});
