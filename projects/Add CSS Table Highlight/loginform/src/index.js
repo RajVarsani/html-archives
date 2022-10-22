801afada1aafb7f1d72858b066c6d84b900a93f7
Kabir Bhagat <84165213+Dev-kabir-ai@users.noreply.github.com>
2022-10-23 01:12:28 +0530

    Initial commit

---
+++ projects/Add CSS Table Highlight/loginform/src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use Parcel to bundle this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
